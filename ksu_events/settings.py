49065856ddc6dffed47e92e9808033d2a35e4d6f
JKenslow <jkenslow@ksu.edu>
2024-10-09 13:28:51 -0500

    setup cas

---
+++ ksu_events/settings.py
@@ -0,0 +1,10 @@
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'django_cas_ng.backends.CASBackend',
+]
+
+CAS_SERVER_URL = 'https://signin.k-state.edu/WebISO/'
+CAS_LOGOUT_COMPLETELY = True
+CAS_REDIRECT_URL = '/'
+LOGIN_URL = 'cas_ng_login'
+LOGOUT_URL = 'cas_ng_logout'
