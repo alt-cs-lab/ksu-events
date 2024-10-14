6527865724fcfb36a34893b58264123eb54b6e42
JKenslow <jkenslow@ksu.edu>
2024-10-14 10:56:49 -0500

    added model mixin from hack

---
+++ ksu_events/models/mixins.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from django.db import models
+
+
+class TimeStampMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True,
+                                      help_text='The time this record was created.')
+    updated_at = models.DateTimeField(auto_now=True,
+                                      help_text='The last time this record was updated.')
+
+    class Meta:
+        abstract = True
