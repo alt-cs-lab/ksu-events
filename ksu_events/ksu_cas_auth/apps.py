04107152c1ea33d47ea00db5578d78a58c636370
JKenslow <jkenslow@ksu.edu>
2024-10-25 09:33:14 -0500

    modified apps.py of submodules

--- ksu_events/ksu_cas_auth/apps.py
+++ ksu_events/ksu_cas_auth/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
 
-class KsuCasAuthConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "ksu_cas_auth"
+class BaseConfig(AppConfig):
+    name = 'ksu_events.ksu_cas_auth'
