15d06e97fdc396a645483af1559761c5841b0bd5
JKenslow <jkenslow@ksu.edu>
2024-11-11 10:35:08 -0600

    added accountAdapter.py

---
+++ ksu_events/oauth2/accountAdapter.py
@@ -0,0 +1,19 @@
+from allauth.account.adapter import DefaultAccountAdapter
+from django.urls import reverse
+# from hackkstate.models import Hackathon
+# from registration.models import Registrations
+
+class AccountAdapter(DefaultAccountAdapter):
+
+    def get_login_redirect_url(self, request):
+        url = super().get_login_redirect_url(request)
+        
+        # if 'login/callback' in request.path:
+            # active_season = Hackathon.objects.get_active_season()
+            # profile = Registrations.objects.get_registration_hackathon(user=request.user, hackathon_id=active_season)
+            # active_season = Registrations.objects.is_active(profile=profile, active_season=active_season)
+
+            # if profile is None or not active_season:
+                # url = reverse('registration:register')
+
+        return url
