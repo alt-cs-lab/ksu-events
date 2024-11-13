f79920a98a11ecfa8f099fcf58ac01d28e1b1a39
Whipple <94202163+g0ggle@users.noreply.github.com>
2024-11-13 11:03:18 -0600

    made big changes to rename things

---
+++ ksu_events/ksu_events/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class KsuEventsConfig(AppConfig):
+    name = 'ksu_events'
--- ksu_events/ksu_events/urls.py
+++ ksu_events/ksu_events/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from ksu_events.base import views
+from ksu_events.ksu_events import views
 
 urlpatterns = [
     path('', views.home, name='home_view'),
