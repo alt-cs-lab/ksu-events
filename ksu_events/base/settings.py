52d24ba5650a3dd8c02c8f7c77c7466c9df75002
Whipple <94202163+g0ggle@users.noreply.github.com>
2024-11-13 10:45:34 -0600

    edited settings.py

--- ksu_events/base/settings.py
+++ ksu_events/base/settings.py
@@ -1,7 +1,13 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            #os.path.join(BASE_DIR, "/base/templates")
+                 ],
         'APP_DIRS': True, 
         'OPTIONS': {
             'context_processors': [
