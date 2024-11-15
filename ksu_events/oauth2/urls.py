360bb74007c0eb942c194c8bb5100f97c8fc08eb
JKenslow <jkenslow@ksu.edu>
2024-11-15 09:44:58 -0600

    changed urls.py

--- ksu_events/oauth2/urls.py
+++ ksu_events/oauth2/urls.py
@@ -2,7 +2,8 @@ from django.urls import path, include
 from allauth.socialaccount.providers.oauth2.urls import default_urlpatterns
 from ksu_events.oauth2.provider import MLHProvider
 
-urlpatterns = [
-    path('', include('allauth.urls')),  # Include all default allauth URLs
-    path('mlh/', include(default_urlpatterns(MLHProvider))),  # Custom MLH OAuth URLs
-]
+# urlpatterns = [
+#     path('', include('allauth.urls')),  # Include all default allauth URLs
+#    path('mlh/', include(default_urlpatterns(MLHProvider))),  # Custom MLH OAuth URLs
+# ]
+urlpatterns = default_urlpatterns(MLHProvider)
