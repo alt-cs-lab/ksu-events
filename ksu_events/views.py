10572ec724d16a9a4d8d70e13d8014ebdbe69655
Ia-in03 <iaincline@outlook.com>
2024-12-02 16:19:30 +0000

    Added code to view that should view event model

--- ksu_events/views.py
+++ ksu_events/views.py
@@ -1,10 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Event
 
 
 def home(request): 
-    return render(request, 'ksu_events/home_page.html')
+    event_models = Event.objects.all()
+
+    context = {
+        'event_models': event_models
+    }
+
+    return render(request, 'ksu_events/home_page.html', context)
     #return HttpResponse("Hello world, this msg is from the events pkg")
 
 
@@ -14,3 +21,4 @@ def redirect(request):
     username = user.username
     
     return HttpResponse(username + " has successfully logged in with KSU CAS Auth.")
+    
