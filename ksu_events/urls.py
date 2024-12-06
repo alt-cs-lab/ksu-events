c3bbec200081587de84ff865428978e278600817
Ia-in03 <65005143+Ia-in03@users.noreply.github.com>
2024-12-06 06:53:24 +0000

    change

--- ksu_events/urls.py
+++ ksu_events/urls.py
@@ -3,5 +3,5 @@ from .views import home, view_models
 
 urlpatterns = [
     path('', home, name='home_view'),
-    path('/models', view_models, name="models_view"),
+    path('models/', view_models, name="models_view"),
 ]
