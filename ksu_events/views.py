764d77d74ea9815eb2fa02cbaeeab051888010b3
Whipple <94202163+g0ggle@users.noreply.github.com>
2024-12-06 16:17:01 +0000

    wip clock might not work

--- ksu_events/views.py
+++ ksu_events/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Event
 
 
-def home(request): 
-    return render(request, 'ksu_events/home_page.html')
+def home(request, event_id): 
+    event = Event.objects.get(pk=event_id)
+    return render(request, 'ksu_events/home_page.html', {'event_start_date': event.event_start_date})
     #return HttpResponse("Hello world, this msg is from the events pkg")
 
 
