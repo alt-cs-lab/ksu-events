744c7e26cdd5832c5f3f50879911a77186eaaf5f
Whipple <94202163+g0ggle@users.noreply.github.com>
2024-12-06 16:18:50 +0000

    reverting clock to previous stage

--- ksu_events/views.py
+++ ksu_events/views.py
@@ -1,12 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from .models import Event
 
 
-def home(request, event_id): 
-    event = Event.objects.get(pk=event_id)
-    return render(request, 'ksu_events/home_page.html', {'event_start_date': event.event_start_date})
+def home(request): 
+    return render(request, 'ksu_events/home_page.html')
     #return HttpResponse("Hello world, this msg is from the events pkg")
 
 
