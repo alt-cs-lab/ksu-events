8c2ee7ca32ff50e13db2b9d54e8dbb580d0f6a59
Ia-in03 <65005143+Ia-in03@users.noreply.github.com>
2025-02-03 20:03:03 +0000

    Started implention model diagram for participants/orgainizers

--- ksu_events/models/__init__.py
+++ ksu_events/models/__init__.py
@@ -1,3 +1,6 @@
 from ksu_events.models.model_events import Event
 from ksu_events.models.model_subevents import SubEvent
 from ksu_events.models.model_users import User
+from ksu_events.models.model_authed_users import Authed_Users
+from ksu_events.models.models_orgainizers import Orgainizers
+from ksu_events.models.models_participants import Participants
---
+++ ksu_events/models/models_participants.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+from ksu_events.models.mixins import TimeStampMixin
+from ksu_events.models.model_authed_users import Authed_Users
+from ksu_events.models.model_events import Event
+
+class Participants(TimeStampMixin, models.Model):
+    user = models.ForeignKey(Authed_Users, on_delete=models.DO_NOTHING)
+    event = models.ForeignKey(Event, on_delete=models.DO_NOTHING)
+
+    institution = models.CharField(max_length=255, blank=False)
+    team = models.CharField(max_length=255)
