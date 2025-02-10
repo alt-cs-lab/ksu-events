e049030a4424dfa3ee0594fde63e45310ce7f65b
Whipple <94202163+g0ggle@users.noreply.github.com>
2025-02-10 09:20:54 +0000

    edited the org dash

--- ksu_events/urls.py
+++ ksu_events/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home, view_models, create_models
+from .views import home, view_models, create_models, edit_event
 
 '''Sets the home and models urls'''
 urlpatterns = [
     path('', home, name='home_view'),
     path('models/', view_models, name="models_view"),
     path('orgdash/', create_models, name="org_view"),
+    path('orgdash/<int:event_id>/', edit_event, name='orgdash'),
 ]
