fc4ae3158a46bc576e433f09a35a0d923e8fbc0c
Ia-in03 <65005143+Ia-in03@users.noreply.github.com>
2025-02-10 18:08:02 +0000

    chg 2

--- ksu_events/migrations/0002_remove_user_is_organizer_user_auth_role_user_event_and_more.py
+++ ksu_events/migrations/0002_remove_user_is_organizer_user_auth_role_user_event_and_more.py
@@ -23,7 +23,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='user',
             name='event',
-            field=models.ForeignKey(default='ORG', on_delete=django.db.models.deletion.CASCADE, to='ksu_events.event'),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='ksu_events.event'),
             preserve_default=False,
         ),
         migrations.AddField(
