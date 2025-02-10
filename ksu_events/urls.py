93999194298990c4a9581d14fd75593d07b3e9f2
Whipple <94202163+g0ggle@users.noreply.github.com>
2025-02-10 10:09:30 +0000

    added delete button

--- ksu_events/urls.py
+++ ksu_events/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, view_models, create_models, edit_event
+from .views import home, view_models, create_models, edit_event, delete_event
 
 '''Sets the home and models urls'''
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path('models/', view_models, name="models_view"),
     path('orgdash/', create_models, name="org_view"),
     path('edit/<int:event_id>/', edit_event, name='edit_event'),
+    path('delete/<int:event_id>/', delete_event, name='delete_event'),
 ]
--- ksu_events/views.py
+++ ksu_events/views.py
@@ -38,34 +38,38 @@ def view_models(request):
 
 @login_required
 def create_models(request):
-    try:
-        if request.method == 'POST':
-            form = EventForm(request.POST)
-            if form.is_valid():
-                form.save()
-                return redirect('organizer_dash')
-        else:
-            form = EventForm()
-        return render(request, 'ksu_events/organizer_dash.html', {'form': form})
-    except AttributeError:
-        return redirect('models_view')
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('organizer_dash')
+    else:
+        form = EventForm()
+    return render(request, 'ksu_events/organizer_dash.html', {'form': form})
 
 @login_required
 def edit_event(request, event_id=None):
-    try:
-        if event_id:  
-            event = get_object_or_404(Event, id=event_id)
-        else:  
-            event = None
+    if event_id:  
+        event = get_object_or_404(Event, id=event_id)
+    else:  
+        event = None
 
-        form = EventForm(request.POST or None, instance=event)
-        if request.method == "POST" and form.is_valid():
-            form.save()
-            return redirect('view_models')
+    form = EventForm(request.POST or None, instance=event)
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        return redirect('view_models')
 
-        return render(request, 'ksu_events/edit_event.html', {'form': form})
-    except AttributeError:
-        return redirect('models_view')
+    return render(request, 'ksu_events/edit_event.html', {'form': form})
+
+@login_required
+def delete_event(request, event_id):
+    event = get_object_or_404(Event, id=event_id)
+
+    if request.method == 'POST':
+        event.delete()  
+        return redirect('view_models')  
+
+    return render(request, 'ksu_events/view_models.html', {'event': event})
 
 '''This method shows that a user has logged in'''
 @login_required
