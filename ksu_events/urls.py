db02be9dfd7bc203a308dd16ebb61558ce4e2c17
Whipple <94202163+g0ggle@users.noreply.github.com>
2025-02-21 17:52:18 +0000

    edited urls.py to match views.py

--- ksu_events/urls.py
+++ ksu_events/urls.py
@@ -4,7 +4,7 @@ from .views import HomeView, ViewModelsView, CreateModelsView, EditEventView
 '''Sets the home and models urls'''
 urlpatterns = [
     path('', HomeView.as_view(), name='home_view'),
-    path('models/', ViewModelsView.as_view(), name="models_view"),
+    path('models/', ViewModelsView.as_view(), name="view_models"),
     path('orgdash/', CreateModelsView.as_view(), name="org_view"),
     path('edit/<int:event_id>/', EditEventView.as_view(), name='edit_event'),
 ]
