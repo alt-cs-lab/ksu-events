e502f08c8c5c4d65dda84717c2445bc8b637d0fa
Whipple <94202163+g0ggle@users.noreply.github.com>
2025-02-21 19:05:36 +0000

    edited urls.py organizer dash

--- ksu_events/urls.py
+++ ksu_events/urls.py
@@ -5,7 +5,7 @@ from .views import HomeView, ViewModelsView, CreateModelsView, EditEventView, Us
 urlpatterns = [
     path('', HomeView.as_view(), name='home_view'),
     path('models/', ViewModelsView.as_view(), name="view_models"),
-    path('orgdash/', CreateModelsView.as_view(), name="org_view"),
+    path('orgdash/', CreateModelsView.as_view(), name="organizer_dash"),
     path('edit/<int:event_id>/', EditEventView.as_view(), name='edit_event'),
     path("profile/", UserProfileView.as_view(), name="user-profile")
 ]
