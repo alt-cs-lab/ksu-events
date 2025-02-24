4d5c5f960a9678674785dbc004405f96ba641257
Ia-in03 <65005143+Ia-in03@users.noreply.github.com>
2025-02-24 06:57:08 +0000

    made models for most reg tables, just need to do user and registration next

---
+++ ksu_events/registration/models/__init__.py
@@ -0,0 +1,6 @@
+# need to add all models here for them to be accessible
+from ksu_events.registration.models.model_ethnicity_options import EthnicityOption
+from ksu_events.registration.models.model_major_options import MajorOption
+from ksu_events.registration.models.model_chaperone import Chaperone
+from ksu_events.registration.models.model_participant_chaperone import ParticipantChaperone
+from ksu_events.registration.models.model_participant import Participant
