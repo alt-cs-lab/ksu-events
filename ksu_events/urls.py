6ee6a0a6eda31caeb923ada700836f7b78dbc386
Whipple <94202163+g0ggle@users.noreply.github.com>
2025-04-01 03:01:18 +0000

    edited url to urls.py

--- ksu_events/urls.py
+++ ksu_events/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('orgdash/', CreateModelsView.as_view(), name="organizer_dash"),
     path('edit/<int:event_id>/', EditEventView.as_view(), name='edit_event'),
     path("profile/", UserProfileView.as_view(), name="user-profile"),
-    path('register/', EventAttendanceView.as_view(), name='register_event'),
+    path('register/', EventAttendanceView.as_view(), name='attend_event'),
     path('participants', ViewParticipantsView.as_view(), name="participants")
 ]
