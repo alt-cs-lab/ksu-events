d2db37bd09a4bf0080b1ae08debf20f4c3eeb66d
Whipple <94202163+g0ggle@users.noreply.github.com>
2025-04-04 17:36:04 +0000

    added some registrations stuff

--- ksu_events/forms.py
+++ ksu_events/forms.py
@@ -1,16 +1,9 @@
 from django import forms
 from django.forms import ModelForm
 
-from .models import Event, EventAttendance
+from .models import Event
 
 class EventForm(ModelForm):
     class Meta:
         model = Event
         fields = ["name", "start_date", "end_date", "registration_start", "registration_end", "location"]
-
-class EventAttendanceForm(forms.ModelForm):
-    event = forms.ModelChoiceField(queryset=Event.objects.all(), empty_label="Select an event")
-
-    class Meta:
-        model = EventAttendance
-        fields = ['event']
--- ksu_events/urls.py
+++ ksu_events/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from ksu_events.views.other_views import HomeView, ViewModelsView, CreateModelsView, EditEventView, UserProfileView, ViewParticipantsView, EventAttendanceView
+from ksu_events.registration.views import RegisterView
+from ksu_events.views.other_views import HomeView, ViewModelsView, CreateModelsView, EditEventView, UserProfileView, ViewParticipantsView
 
 # Sets the home and models urls
 urlpatterns = [
@@ -8,6 +9,6 @@ urlpatterns = [
     path('orgdash/', CreateModelsView.as_view(), name="organizer_dash"),
     path('edit/<int:event_id>/', EditEventView.as_view(), name='edit_event'),
     path("profile/", UserProfileView.as_view(), name="user-profile"),
-    path('register/', EventAttendanceView.as_view(), name='attend_event'),
+    path('register/<int:event_id>/', RegisterView.as_view(), name='register'),
     path('participants', ViewParticipantsView.as_view(), name="participants")
 ]
