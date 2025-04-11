125dc76d10ac416f0fe4175b6bcd8de0799188ee
JKenslow <jkenslow@ksu.edu>
2025-04-11 12:06:27 -0500

    changed registration form

--- ksu_events/forms.py
+++ ksu_events/forms.py
@@ -8,9 +8,34 @@ class EventForm(ModelForm):
         model = Event
         fields = ["name", "start_date", "end_date", "registration_start", "registration_end", "location"]
 
-class EventAttendanceForm(forms.ModelForm):
-    event = forms.ModelChoiceField(queryset=Event.objects.all(), empty_label="Select an event")
-
+class RegistrationForm(forms.ModelForm):
+    event = forms.ModelChoiceField(
+        queryset=Event.objects.all(), 
+        empty_label="Select an event",
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+    
     class Meta:
         model = Registration
-        fields = ['event']
+        fields = [
+            'event', 
+            'country', 
+            'dietary_restrictions', 
+            'phone_number',
+            'ethnicity',
+            'is_minor',
+            'participation',
+            'shirt_size',
+            'year_in_school'
+        ]
+        # Exclude the user field since we'll set it automatically
+        exclude = ['user']
+        
+        widgets = {
+            'dietary_restrictions': forms.Textarea(attrs={'rows': 3, 'class': 'form-control'}),
+            'phone_number': forms.TextInput(attrs={'class': 'form-control'}),
+            'ethnicity': forms.TextInput(attrs={'class': 'form-control'}),
+            'participation': forms.Select(attrs={'class': 'form-control'}),
+            'shirt_size': forms.Select(attrs={'class': 'form-control'}),
+            'year_in_school': forms.Select(attrs={'class': 'form-control'}),
+        }
--- ksu_events/urls.py
+++ ksu_events/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from ksu_events.views.other_views import HomeView, ViewModelsView, CreateModelsView, EditEventView, UserProfileView, ViewParticipantsView, EventAttendanceView
+from ksu_events.views.other_views import HomeView, ViewModelsView, CreateModelsView, EditEventView, UserProfileView, ViewParticipantsView, EventRegistrationView
 
 # Sets the home and models urls
 urlpatterns = [
@@ -8,6 +8,6 @@ urlpatterns = [
     path('orgdash/', CreateModelsView.as_view(), name="organizer_dash"),
     path('edit/<int:event_id>/', EditEventView.as_view(), name='edit_event'),
     path("profile/", UserProfileView.as_view(), name="user-profile"),
-    path('register/', EventAttendanceView.as_view(), name='attend_event'),
+    path('register/', EventRegistrationView.as_view(), name='register_event'),
     path('participants', ViewParticipantsView.as_view(), name="participants")
 ]
