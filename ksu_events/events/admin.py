185ad38f2b74d12c083c2041d1497de0c8c78773
JKenslow <jkenslow@ksu.edu>
2025-04-18 16:18:19 -0500

    trying to make password optional in admin panel

--- ksu_events/events/admin.py
+++ ksu_events/events/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Event, SubEvent, User
 
+class CASUserAdmin(UserAdmin):
+    # Override save_model to make password field optional
+    def save_model(self, request, obj, form, change):
+        # Don't require a password when saving
+        if not obj.password:
+            obj.set_unusable_password()
+        super().save_model(request, obj, form, change)
+
 # Registered models.
+admin.site.register(User, CASUserAdmin)
 admin.site.register(Event)
 admin.site.register(SubEvent)
-admin.site.register(User)
