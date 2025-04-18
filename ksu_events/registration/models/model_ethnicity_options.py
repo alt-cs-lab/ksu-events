f913b44d3a2f5cb6a89c8e29ce78c51c1166864e
JKenslow <jkenslow@ksu.edu>
2025-04-18 13:35:38 -0500

    increased ethnicity length

--- ksu_events/registration/models/model_ethnicity_options.py
+++ ksu_events/registration/models/model_ethnicity_options.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class EthnicityOption(models.Model):
-    value = models.CharField(max_length=20, blank=False, null=False)
+    value = models.CharField(max_length=50, blank=False, null=False)
 
     def __str__(self):
         return  self.value
