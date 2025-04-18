079e8e17991747da1785ceff6795fda07103306e
JKenslow <jkenslow@ksu.edu>
2025-04-18 15:50:16 -0500

    cleaned up registration app

--- ksu_events/registration/models/model_ethnicity_options.py
+++ ksu_events/registration/models/model_ethnicity_options.py
@@ -1,8 +1,7 @@
 from django.db import models
 
-
 class EthnicityOption(models.Model):
     value = models.CharField(max_length=50, blank=False, null=False)
 
     def __str__(self):
-        return  self.value
+        return  f"{self.value}"
